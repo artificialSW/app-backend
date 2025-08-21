07b731a67ff09a89afae1e2e981c0de19da72181
scyth0425 <scythe0425@naver.com>
2025-08-21 13:43:08 +0900

    add DTO file

---
+++ artificialsw-backend/src/main/java/org/dcode/artificialswbackend/community/entity/Comment.java
@@ -0,0 +1,89 @@
+package org.dcode.artificialswbackend.community.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private Long questionId;
+    private Long writer;
+    private String content;
+    private Long replyTo;
+    private int likes;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Comment() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Long getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(Long questionId) {
+        this.questionId = questionId;
+    }
+
+    public Long getWriter() {
+        return writer;
+    }
+
+    public void setWriter(Long writer) {
+        this.writer = writer;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Long getReplyTo() {
+        return replyTo;
+    }
+
+    public void setReplyTo(Long replyTo) {
+        this.replyTo = replyTo;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
