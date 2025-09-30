03547cd9fe1a93f1bf2e5e8edf8b7f583199fc14
scyth0425 <scythe0425@naver.com>
2025-10-01 00:39:50 +0900

    edit sql; edit typo

--- artificialsw-backend/src/main/java/org/dcode/artificialswbackend/db/asw.sql
+++ artificialsw-backend/src/main/java/org/dcode/artificialswbackend/db/asw.sql
@@ -146,7 +146,7 @@ CREATE TABLE `tree` (
                         KEY `archive_id` (`archive_id`),
                         KEY `fk_tree_family` (`family_id`),
                         CONSTRAINT `fk_tree_family` FOREIGN KEY (`family_id`) REFERENCES `families` (`id`),
-                        CONSTRAINT `tree_ibfk_1` FOREIGN KEY (`archive_id`) REFERENCES `archives` (`id`)
+                        CONSTRAINT `tree_ibfk_1` FOREIGN KEY (`archive_id`) REFERENCES `island_archive` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- flowers 테이블 (tree_id 통해 family_id 간접관리)
