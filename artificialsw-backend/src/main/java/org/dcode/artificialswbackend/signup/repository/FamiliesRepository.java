de97399136874bee453f1687f73ca731b6f13e26
scyth0425 <scythe0425@naver.com>
2025-09-30 17:27:42 +0900

    edit login

--- artificialsw-backend/src/main/java/org/dcode/artificialswbackend/signup/repository/FamiliesRepository.java
+++ artificialsw-backend/src/main/java/org/dcode/artificialswbackend/signup/repository/FamiliesRepository.java
@@ -4,7 +4,9 @@ import org.dcode.artificialswbackend.signup.entity.Families;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FamiliesRepository extends JpaRepository<Families, Long> {
-
+    Optional<Families> findByVerificationCode(String verificationCode);
 }
