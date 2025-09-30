89cfbe39f06ff84efe87f86b3ec8fcac4757a874
scyth0425 <scythe0425@naver.com>
2025-09-30 14:18:19 +0900

    add security

---
+++ artificialsw-backend/src/main/java/org/dcode/artificialswbackend/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package org.dcode.artificialswbackend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/signup", "/api/login").permitAll()
+                        .anyRequest().authenticated()
+                ); // JWT 인증 필터 등록 등 추가 설정 가능
+        return http.build();
+    }
+}
