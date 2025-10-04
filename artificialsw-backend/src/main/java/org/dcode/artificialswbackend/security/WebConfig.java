c2b2a12f91ac5bc13e366e3c5107f8ddbf7024be
scyth0425 <scythe0425@naver.com>
2025-10-05 01:17:07 +0900

    edit properties; ec2 url link......

---
+++ artificialsw-backend/src/main/java/org/dcode/artificialswbackend/security/WebConfig.java
@@ -0,0 +1,16 @@
+package org.dcode.artificialswbackend.security; // SecurityConfig와 같은 패키지로 지정
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // 실제 배포용에선 특정 Origin만 등록하세요
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
