4072ad36c92d89a0359c9e7404cce63f067cc47b
pataegonia <clue9986@gmail.com>
2025-10-13 10:00:15 +0900

    adding score system

--- artificialsw-backend/src/main/java/org/dcode/artificialswbackend/archive/repository/IslandArchivesRepository.java
+++ artificialsw-backend/src/main/java/org/dcode/artificialswbackend/archive/repository/IslandArchivesRepository.java
@@ -2,11 +2,17 @@ package org.dcode.artificialswbackend.archive.repository;
 
 import org.dcode.artificialswbackend.archive.entity.IslandArchives;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface IslandArchivesRepository extends JpaRepository<IslandArchives, Long> {
     Optional<IslandArchives> findByFamilyIdAndYearAndMonthAndPeriod(Long familyId, Integer year, Integer month, Integer period);
     Optional<IslandArchives> findByIdAndFamilyId(Long id, Long familyId);
-
+    
+    @Modifying
+    @Query("UPDATE IslandArchives i SET i.communityScore = CASE WHEN i.communityScore < 10 THEN i.communityScore + 1 ELSE 10 END WHERE i.id = :archiveId")
+    int incrementCommunityScore(@Param("archiveId") Long archiveId);
 }
