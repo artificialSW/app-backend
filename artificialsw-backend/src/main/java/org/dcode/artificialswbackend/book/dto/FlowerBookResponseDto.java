16f403d36ef74d9a95957c389dac3276f28206da
pataegonia <clue9986@gmail.com>
2025-10-19 15:57:58 +0900

    change response of api

--- artificialsw-backend/src/main/java/org/dcode/artificialswbackend/book/dto/FlowerBookResponseDto.java
+++ artificialsw-backend/src/main/java/org/dcode/artificialswbackend/book/dto/FlowerBookResponseDto.java
@@ -1,51 +1,21 @@
 package org.dcode.artificialswbackend.book.dto;
 
-import java.time.LocalDateTime;
+import java.util.List;
 
 public class FlowerBookResponseDto {
-    private String flowerName; // 한국어 꽃 이름
-    private String flowerType; // 영어 타입 (enum value)
-    private Boolean unlocked;
-    private LocalDateTime unlockedAt;
+    private List<Integer> resolvedFlowers;
     
     public FlowerBookResponseDto() {}
     
-    public FlowerBookResponseDto(String flowerName, String flowerType, Boolean unlocked, LocalDateTime unlockedAt) {
-        this.flowerName = flowerName;
-        this.flowerType = flowerType;
-        this.unlocked = unlocked;
-        this.unlockedAt = unlockedAt;
+    public FlowerBookResponseDto(List<Integer> resolvedFlowers) {
+        this.resolvedFlowers = resolvedFlowers;
     }
     
-    public String getFlowerName() {
-        return flowerName;
+    public List<Integer> getResolvedFlowers() {
+        return resolvedFlowers;
     }
     
-    public void setFlowerName(String flowerName) {
-        this.flowerName = flowerName;
-    }
-    
-    public String getFlowerType() {
-        return flowerType;
-    }
-    
-    public void setFlowerType(String flowerType) {
-        this.flowerType = flowerType;
-    }
-    
-    public Boolean getUnlocked() {
-        return unlocked;
-    }
-    
-    public void setUnlocked(Boolean unlocked) {
-        this.unlocked = unlocked;
-    }
-    
-    public LocalDateTime getUnlockedAt() {
-        return unlockedAt;
-    }
-    
-    public void setUnlockedAt(LocalDateTime unlockedAt) {
-        this.unlockedAt = unlockedAt;
+    public void setResolvedFlowers(List<Integer> resolvedFlowers) {
+        this.resolvedFlowers = resolvedFlowers;
     }
 }
