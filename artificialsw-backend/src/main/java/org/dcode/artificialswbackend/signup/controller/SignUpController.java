8232e5f99aa60ff3035ccb54a8e1e4fd323f6d2d
scyth0425 <scythe0425@naver.com>
2025-10-21 15:01:01 +0900

    sign up 로직 통합

--- artificialsw-backend/src/main/java/org/dcode/artificialswbackend/signup/controller/SignUpController.java
+++ artificialsw-backend/src/main/java/org/dcode/artificialswbackend/signup/controller/SignUpController.java
@@ -7,6 +7,7 @@ import org.dcode.artificialswbackend.archive.repository.IslandArchivesRepository
 import org.dcode.artificialswbackend.signup.dto.LoginRequestDto;
 import org.dcode.artificialswbackend.signup.dto.LoginResponseDto;
 import org.dcode.artificialswbackend.signup.dto.SignUpRequestDto;
+import org.dcode.artificialswbackend.signup.entity.Families;
 import org.dcode.artificialswbackend.signup.service.SignUpService;
 import org.dcode.artificialswbackend.util.JwtUtil;
 import org.springframework.http.ResponseEntity;
@@ -32,10 +33,16 @@ public class SignUpController {
 
     @PostMapping("/api/signup")
     public ResponseEntity<String> signup(@RequestBody SignUpRequestDto request) {
-        signUpService.signup(request);
-        return ResponseEntity.ok("회원가입 완료");
-    }
+        Families family = signUpService.signup(request);
 
+        if (request.getFamilyVerificationCode() == null || request.getFamilyVerificationCode().isEmpty()) {
+            // 신규 가족 생성 후 회원가입
+            return ResponseEntity.ok("회원가입 완료 - 새로운 가족 생성");
+        } else {
+            // 기존 가족 코드로 회원가입
+            return ResponseEntity.ok("회원가입 완료 - 기존 가족 코드로 가입되었습니다.");
+        }
+    }
     @PostMapping("/api/login")
     public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto request) {
         // 1. 로그인 인증 및 JWT 발급
--- artificialsw-backend/src/main/java/org/dcode/artificialswbackend/signup/dto/LoginResponseDto.java
+++ artificialsw-backend/src/main/java/org/dcode/artificialswbackend/signup/dto/LoginResponseDto.java
@@ -1,23 +1,13 @@
 package org.dcode.artificialswbackend.signup.dto;
 
-import lombok.Setter;
-
 public class LoginResponseDto {
     private final String token;
-    @Setter
-    private Long archiveId;
 
     public LoginResponseDto(String token, Long archiveId) {
         this.token = token;
-        this.archiveId = archiveId;
     }
 
     public String getToken() {
         return token;
     }
-
-    public Long getArchiveId() {
-        return archiveId;
-    }
-
 }
