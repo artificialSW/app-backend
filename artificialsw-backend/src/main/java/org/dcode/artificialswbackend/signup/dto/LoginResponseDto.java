40869dac38650921309286ab8d3768c81c76473d
pataegonia <clue9986@gmail.com>
2025-11-20 12:42:10 +0900

    adding archive id when login

--- artificialsw-backend/src/main/java/org/dcode/artificialswbackend/signup/dto/LoginResponseDto.java
+++ artificialsw-backend/src/main/java/org/dcode/artificialswbackend/signup/dto/LoginResponseDto.java
@@ -2,12 +2,18 @@ package org.dcode.artificialswbackend.signup.dto;
 
 public class LoginResponseDto {
     private final String token;
+    private final Long archiveId;
 
     public LoginResponseDto(String token, Long archiveId) {
         this.token = token;
+        this.archiveId = archiveId;
     }
 
     public String getToken() {
         return token;
+    }
+
+    public Long getArchiveId() {
+        return archiveId;
     }
 }
